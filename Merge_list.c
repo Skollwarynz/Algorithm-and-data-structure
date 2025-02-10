cbc1757c4f49def33f0f0aa156ec5718d488557e
Skollwarynz <zante342@gmail.com>
2025-02-10 15:00:16 +0100

    Added merge for linked lists
    Added function for recoursive merge of 2 linked list. The function need to add library to linked list structure and compare function for void* library.

--- Merge_list.c
+++ Merge_list.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+list merge(list l, list m){
+  if(!l)
+     return m;
+  else if(!l)
+     return l;
+  else 
+     if(l->n < m->n){
+        l = merge(l->next,m);
+        return l;
+     else{
+        m = merge(l,m->next);
+        return m;
+     }     
+}
